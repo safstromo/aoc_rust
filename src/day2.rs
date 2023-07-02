eebc8bc3b37ddda6deeb3b891dbf3e1ccafcb31b
Oliver Säfström <safstrom.oliver@gmail.com>
2023-07-02 13:33:08 +0200

    Day2 part 2

--- src/day2.rs
+++ src/day2.rs
@@ -18,7 +18,7 @@ const ROCK_SCORE: u32 = 1;
 const PAPER_SCORE: u32 = 2;
 
 
-fn day2() -> u32 {
+fn day2_part1() -> u32 {
     let input = common::get_file("src/input/day2.txt").expect("failed to read file");
     let rounds: Vec<(&str, &str)> = input.lines()
         .map(|line| line.split_once(' ').unwrap())
@@ -27,12 +27,12 @@ fn day2() -> u32 {
 
     for round in rounds {
         match round {
-            (OPPONENT_ROCK, PAPER) => score += WIN,
-            (OPPONENT_PAPER, SCISSORS) => score += WIN,
-            (OPPONENT_SCISSORS, ROCK) => score += WIN,
-            (OPPONENT_PAPER, PAPER) => score += DRAW,
-            (OPPONENT_SCISSORS, SCISSORS) => score += DRAW,
-            (OPPONENT_ROCK, ROCK) => score += DRAW,
+            (OPPONENT_ROCK, PAPER) => add_win_score(&mut score),
+            (OPPONENT_PAPER, SCISSORS) => add_win_score(&mut score),
+            (OPPONENT_SCISSORS, ROCK) => add_win_score(&mut score),
+            (OPPONENT_PAPER, PAPER) => add_draw_score(&mut score),
+            (OPPONENT_SCISSORS, SCISSORS) => add_draw_score(&mut score),
+            (OPPONENT_ROCK, ROCK) => add_draw_score(&mut score),
             _ => ()
         }
 
@@ -47,12 +47,70 @@ fn day2() -> u32 {
     score
 }
 
+fn day2_part2() -> u32 {
+    let mut score = 0;
+    let input = common::get_file("src/input/day2.txt").expect("failed to read file");
+    let rounds: Vec<(&str, &str)> = input.lines()
+        .map(|line| line.split_once(' ').unwrap())
+        .collect();
+    for round in rounds {
+        match round.1 {
+            "Z" => add_win_score(&mut score),
+            "Y" => add_draw_score(&mut score),
+            _ => ()
+        }
+
+        match round.1 {
+            "X" => {
+                match round.0 {
+                    OPPONENT_ROCK => score += SCISSORS_SCORE,
+                    OPPONENT_SCISSORS => score += PAPER_SCORE,
+                    OPPONENT_PAPER => score += ROCK_SCORE,
+                    _ => ()
+                }
+            }
+            "Y" => {
+                match round.0 {
+                    OPPONENT_ROCK => score += ROCK_SCORE,
+                    OPPONENT_SCISSORS => score += SCISSORS_SCORE,
+                    OPPONENT_PAPER => score += PAPER_SCORE,
+                    _ => ()
+                }
+            }
+            "Z" => {
+                match round.0 {
+                    OPPONENT_ROCK => score += PAPER_SCORE,
+                    OPPONENT_SCISSORS => score += ROCK_SCORE,
+                    OPPONENT_PAPER => score += SCISSORS_SCORE,
+                    _ => ()
+                }
+            }
+            _ => ()
+        }
+    }
+    score
+}
+
+fn add_draw_score(score: &mut u32) {
+    *score += DRAW
+}
+
+fn add_win_score(score: &mut u32) {
+    *score += WIN
+}
+
 
 mod tests {
-    use crate::day2::day2;
+    use crate::day2::{day2_part1, day2_part2};
 
     #[test]
     fn day2_part1_test() {
-        assert_eq!(day2(), 14375)
+        assert_eq!(day2_part1(), 14375)
+    }
+
+    #[test]
+    fn day2_part2_test() {
+        assert_eq!(day2_part2(), 10274)
     }
 }
+
